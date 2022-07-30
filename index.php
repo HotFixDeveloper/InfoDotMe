404fe74f5dbc9c7b0b1f09c1b2ffc889789788f3
Elliot Alderson <110250872+HotFixDeveloper@users.noreply.github.com>
2022-07-30 23:34:45 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+      $ClientUA = $_SERVER['HTTP_USER_AGENT'];
+      $ClientIP = $_SERVER['REMOTE_ADDR'];
+
+
+      echo "
+      <div class="user-agent">
+      User Agent - {$ClientUA}
+      </div>
+      ";
+
+      echo "
+      <div class="ip"> IP-адрес - {$ClientIP}
+      </div>
+      ";
+?>
